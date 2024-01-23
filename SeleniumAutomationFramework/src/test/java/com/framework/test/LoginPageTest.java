1df79eb0c9b7ce0ce9c197911c7a74551955ca17
Rohit <rk8459579@gmail.com>
2024-01-24 00:59:54 +0530

    Commit-3 Starting to update the framework with LoginPageTest class

---
+++ SeleniumAutomationFramework/src/test/java/com/framework/test/LoginPageTest.java
@@ -0,0 +1,13 @@
+package com.framework.test;
+
+import org.testng.annotations.Test;
+
+public class LoginPageTest {
+	
+@Test
+
+public void test1() {
+	System.setProperty("webdriver.chrome.driver", "");
+}
+
+}
